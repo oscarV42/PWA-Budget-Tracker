7d0ae908008a98d853136f9a251559243c2145c7
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-11-05 02:43:41 -0400

    adding an event listener when service-worker is initally installed, cache FILES_TO_CACHE

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,3 +5,15 @@ const FILES_TO_CACHE = [
     'https://fonts.googleapis.com/css?family=Istok+Web|Montserrat:800&display=swap',
     'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css'
 ]
+
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+      caches
+        .open(PRECACHE)
+        .then((cache) => cache.addAll(FILES_TO_CACHE))
+        .then(self.skipwaiting())
+  )
+})
