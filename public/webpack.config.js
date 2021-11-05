92d604842213a9ba13b726f47e5a2f1ef29d8387
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-11-05 01:57:52 -0400

    including plugins to perfrom wider range of tasks

--- public/webpack.config.js
+++ public/webpack.config.js
@@ -11,18 +11,36 @@ const config = {
     },
     mode: 'development',
     module: {
-        rules: [
-         {
-            test: /\.js$/,
-            exclude: /node_modules/,
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
             use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env']
-                }
+              loader: 'babel-loader',
+              options: {
+              presets: ['@babel/preset-env']
             }
-         }
+          }
+        }
+      ]
+    },
+    plugins: [
+      new WebpackPwaManifest({
+        fingerprints: false,
+        name: 'Budegt App',
+        short_name: 'Budget', 
+        description: 'A budget tracking application.',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        'theme-color': '#ffffff',
+        start_url: '/',
+        icons: [
+          {
+            src: path.resolve('assets/images/icons/icon-192x192.png'),
+            sizes: [192, 512],
+            destination: path.join('assets', 'icons')
+          }
+        ]
+      })  
     ]
-    }
-
 }
