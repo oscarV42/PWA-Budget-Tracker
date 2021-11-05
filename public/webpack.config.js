822f201c43f66cbed131936feae79dc2faa3ef37
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-11-05 00:55:55 -0400

    declaring output property, telling webpack where to emit the bundles

--- public/webpack.config.js
+++ public/webpack.config.js
@@ -4,5 +4,9 @@ const path = require('path');
 const config = {
     entry: {
         app: './assets/js/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, '/dist'),
+        filename: 'bundle.js'
     }
 }
