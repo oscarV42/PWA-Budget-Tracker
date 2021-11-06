5a683f2dff91ec8e013f9abab92051daa5685a24
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-11-06 03:51:29 -0400

    creating function that handles sending data in IndexedDB too the MongoDB database and clears the records after

--- public/js/IndexedDB.js
+++ public/js/IndexedDB.js
@@ -24,3 +24,29 @@ request.onsuccess = ({ target }) => {
 request.onerror = (event) => {
     console.log('It broke... ', event.target.errorCode)
 }
+
+const checkDB = () => {
+  const transaction = db.transaction(["pending"], ["readwrite"]);
+  const store = transaction.objectStore('pending');
+  const getAll = store.getAll();
+
+  getAll.onsuccess = () => {
+    if(getAll.result.length > 0) {
+      fetch('/api/transaction/bulk', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: 'application/json'
+      })
+      .then(response => {
+          return response.json();
+      })
+      .then(() => {
+          // delete records if successful
+          store.clear();
+      })
+    } 
+  }
+}
+
+// listening for application coming back online
+window.addEventListener('online', checkDB)
