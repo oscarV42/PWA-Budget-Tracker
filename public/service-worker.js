17c56d52fd19900e252e6949efd00dba1fb1fa92
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-11-08 22:46:12 -0500

    updating files to cache array

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,13 +1,13 @@
 const FILES_TO_CACHE = [
-    '/',
-    '/js/IndexedDB.js',
-    '/js/index.js',
-    '/manifest.json',
-    '/css/styles.css',
-    // "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
-    // "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+    "/",
+    "/js/IndexedDB.js",
+    "/js/index.js",
+    "/manifest.json",
+    "/css/styles.css",
     "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png"
+    "/icons/icon-512x512.png",
+    // "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+    // "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
 ]
 
 const CACHE_NAME = 'cite-cache-v2';
